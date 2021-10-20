f85744daefc52aa818cb5e2136c3c44c5c0f405f
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2021-10-20 14:52:17 -0600

    adjust headers for cors

--- components/form/form_component.js
+++ components/form/form_component.js
@@ -30,7 +30,11 @@ export const FormComponent = () => {
       body: JSON.stringify(wineInputs),
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*"
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": "true",
+        "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+        "Access-Control-Allow-Headers":
+          "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
       },
     };
 
@@ -67,7 +71,10 @@ export const FormComponent = () => {
           <div className="p-7">
             <div className="text-center pb-4">
               <div className="text-xl font-extralight">
-                choose prediction algorithm <br/> <span className="text-sm font-normal">(random forest is recommended)</span> 
+                choose prediction algorithm <br />{" "}
+                <span className="text-sm font-normal">
+                  (random forest is recommended)
+                </span>
               </div>
             </div>
             <label
@@ -107,9 +114,7 @@ export const FormComponent = () => {
           </div>
         </div>
         <div className="text-center pt-8">
-          <div className="text-xl font-extralight">
-            adjust inputs
-          </div>
+          <div className="text-xl font-extralight">adjust inputs</div>
         </div>
         <div className="flex flex-wrap flex-row content-start justify-evenly pt-5 ">
           <div className="p-7">
