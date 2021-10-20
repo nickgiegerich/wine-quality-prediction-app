8448b76448b5be56666da3f3af4e5b9fe640ce4c
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2021-10-20 14:35:57 -0600

    fix url

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     DEVELOPMENT_BASE_URL: 'http://127.0.0.1:8000',
-    PRODUCTION_BASE_URL: 'https://wine-quality-ml-api.herokuapp.com/'
+    PRODUCTION_BASE_URL: 'https://wine-quality-ml-api.herokuapp.com'
   },
   reactStrictMode: true,
   webpack(config) {
