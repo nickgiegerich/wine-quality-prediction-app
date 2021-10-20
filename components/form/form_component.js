c7dcbb3011a36d0540c36ac4189cdbfacc56f9a6
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2021-10-20 14:56:52 -0600

    adjust headers for cors

--- components/form/form_component.js
+++ components/form/form_component.js
@@ -31,10 +31,8 @@ export const FormComponent = () => {
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-        "Access-Control-Allow-Headers":
-          "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers",
+        "Access-Control-Allow-Methods": "POST",
+        "Access-Control-Allow-Headers": "Content-Type",
       },
     };
 
