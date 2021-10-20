f9d10ea327067a023616b93ad0c1891a08aadf7b
Nick Giegerich <nickgiegerich@u.boisestate.edu>
2021-10-20 14:57:48 -0600

    adjust headers

--- components/form/form_component.js
+++ components/form/form_component.js
@@ -32,7 +32,7 @@ export const FormComponent = () => {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "POST",
-        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
       },
     };
 
